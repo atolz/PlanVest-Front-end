d72086bbf86bbfd9d9b83cc372f10db83c47a1e7
Atolagbe Elisha <atolagbeelisha001@gmail.com>
2022-09-07 00:25:13 +0100

    little-padding-fix

--- components/layouts/AppLayout.js
+++ components/layouts/AppLayout.js
@@ -7,7 +7,7 @@ const AppLayout = ({ children }) => {
   return (
     <div className="flex h-screen w-screen">
       <Sidebar></Sidebar>
-      <main style={{ width: "calc(100% - 26.2rem)" }} className=" bg-pv_bg overflow-y-scroll scroll_hide pb-[3.2rem]">
+      <main style={{ width: "calc(100% - 262px)" }} className=" bg-pv_bg overflow-y-scroll scroll_hide pb-[3.2rem]">
         <Header></Header>
         <AppContainer className={"pt-[2rem]"}>{children}</AppContainer>
       </main>
--- components/layouts/PlainContainer.js
+++ components/layouts/PlainContainer.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const PlainContainer = ({ children, className }) => {
-  return <div className={` bg-white p-[2.4rem] px-[2.4rem] rounded-secondary h-max w-full ${className}`}>{children}</div>;
+  return <div className={` bg-white p-[2.6rem] px-[2.6rem] rounded-secondary h-max w-full ${className}`}>{children}</div>;
 };
 
 export default PlainContainer;
